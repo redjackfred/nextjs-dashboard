ffef7fb7c8e4c65d5e28936545e93b39958673a0
Pei-Wen Tang (Vincent) <redjackfred@gmail.com>
2024-06-19 21:30:58 -0700

    Fix

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -5,17 +5,17 @@ import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
+  const params = new URLSearchParams(searchParams);
+  const pathname = usePathname();
+  const { replace } = useRouter();
 
-  function handleSearch(term: string) {
-    const params = new URLSearchParams(searchParams);
-    const pathname = usePathname();
-    const { replace } = useRouter();
+  function handleSearch(term: string) {    
     if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
-    replace(`${pathname}?${params.toString()}`);
+    replace(`${pathname}?${params.toString()}`);    
   }
 
   return (
